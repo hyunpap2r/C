fe3a31631130dd872411e87c3c49a475797b3fd4
hyunpaper <hyunpaper5170@naver.com>
2022-06-07 14:44:34 +0900

    struct

--- main.cpp
+++ main.cpp
@@ -1,29 +1,100 @@
-#include<iostream>
+#include <iostream>
+// #include <cstring>
+using namespace std;
 
 // int num = 10;
 
-// int increase()
+// int increase() ////// 함수 //////
 // {
 //     num += 5;
 // }
-float ABs(float f)
-{
-    if (f < 0.0f)
-        return -f;
-    else
-        return f;
-}
+        //////함수//////
+// float ABs(float f)   
+// {
+//     if (f < 0.0f)
+//         return -f;
+//     else
+//         return f;
+// }
 
+        //함수//
 
-bool Equal(float f1, float f2, float epsilon){
-    return ABs(f1 - f2) <= epsilon;
-}
+// bool Equal(float f1, float f2, float epsilon){ 
+//     return ABs(f1 - f2) <= epsilon;
+// }
 
 int main() {
-//     increase();
-//     std::cout << num << std::endl;
+// //     increase();
+// //     std::cout << num << std::endl;
    
-    if(Equal(3.0f, 1.0f + 2.0f, 0.00001f))
-        std::cout << "same" << std::endl;
-    return 0;
-}
+//     if(Equal(3.0f, 1.0f + 2.0f, 0.00001f))
+//         std::cout << "same" << std::endl;
+//     return 0;
+
+// char ch = 'A';
+// cout << static_cast<int>(ch) << " " <<(int)ch << endl;
+
+// int a = 10;
+// int b = 3;
+
+// int c = a + b;
+// int d = a - b;
+// int f = a * b;
+// int g = a / b;
+// int e = a % b;
+
+// cout << "c : " << c << endl;
+// cout << "d : " << d << endl;
+// cout << "f : " << f << endl;
+// cout << "g : " << g << endl;
+// cout << "e : " << e << endl;
+
+// char a[] = {"hello"};
+
+// cout << a[0] << endl;
+
+// return 0;
+
+// const int Size = 15;
+
+// char name1[Size];
+// char name2[Size] = "c++programing";
+
+// cout << "hi" << endl;
+// cout << "name : " << name2 << "\n";
+// cout << "your name?\n";
+// cin.getline(name1, Size);
+// cout << "hi " << name1;
+
+// char char1[20];
+// char char2[20] = "jauar";
+
+// string str1;
+// string str2 = "pan da";
+
+// str1 = str2;
+// cout << str1[0] <<endl;
+    
+ struct my
+ {
+     string name;
+     string position;
+     int age;
+     int height;
+
+ };
+
+    my A = {
+            "son",
+            "mid",
+            29,
+            181
+};
+
+
+    cout << A.name << endl;
+    cout << A.position << endl;
+    cout << A.age << endl;
+    cout << A.height << endl;
+    
+    }
