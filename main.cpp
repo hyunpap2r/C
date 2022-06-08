4f002715b0e04422ab3ae5750be3ee6d20c11711
hyunpaper <hyunpaper5170@naver.com>
2022-06-08 14:52:04 +0900

    new and delete

--- main.cpp
+++ main.cpp
@@ -1,93 +1,169 @@
 #include <iostream>
-// #include <cstring>
+
 using namespace std;
+// #include <cstring>
 
-// int num = 10;
+        // int num = 10;
 
-// int increase() ////// 함수 //////
-// {
-//     num += 5;
-// }
-        //////함수//////
-// float ABs(float f)   
-// {
-//     if (f < 0.0f)
-//         return -f;
-//     else
-//         return f;
-// }
+        // int increase() ////// 함수 //////
+        // {
+        //     num += 5;
+        // }
+                //////함수//////
+        // float ABs(float f)   
+        // {
+        //     if (f < 0.0f)
+        //         return -f;
+        //     else
+        //         return f;
+        // }
 
-        //함수//
+                //함수//
 
-// bool Equal(float f1, float f2, float epsilon){ 
-//     return ABs(f1 - f2) <= epsilon;
-// }
+        // bool Equal(float f1, float f2, float epsilon){ 
+        //     return ABs(f1 - f2) <= epsilon;
+        // }
 
 int main() {
-// //     increase();
-// //     std::cout << num << std::endl;
-   
-//     if(Equal(3.0f, 1.0f + 2.0f, 0.00001f))
-//         std::cout << "same" << std::endl;
-//     return 0;
+        // //     increase();
+        // //     std::cout << num << std::endl;
+        
+        //     if(Equal(3.0f, 1.0f + 2.0f, 0.00001f))
+        //         std::cout << "same" << std::endl;
+        //     return 0;
 
-// char ch = 'A';
-// cout << static_cast<int>(ch) << " " <<(int)ch << endl;
+        // char ch = 'A';
+        // cout << static_cast<int>(ch) << " " <<(int)ch << endl;
 
-// int a = 10;
-// int b = 3;
+        // int a = 10;
+        // int b = 3;
 
-// int c = a + b;
-// int d = a - b;
-// int f = a * b;
-// int g = a / b;
-// int e = a % b;
+        // int c = a + b;
+        // int d = a - b;
+        // int f = a * b;
+        // int g = a / b;
+        // int e = a % b;
 
-// cout << "c : " << c << endl;
-// cout << "d : " << d << endl;
-// cout << "f : " << f << endl;
-// cout << "g : " << g << endl;
-// cout << "e : " << e << endl;
+        // cout << "c : " << c << endl;
+        // cout << "d : " << d << endl;
+        // cout << "f : " << f << endl;
+        // cout << "g : " << g << endl;
+        // cout << "e : " << e << endl;
 
-// char a[] = {"hello"};
+        // char a[] = {"hello"};
 
-// cout << a[0] << endl;
+        // cout << a[0] << endl;
 
-// return 0;
+        // return 0;
 
-// const int Size = 15;
+        // const int Size = 15;
 
-// char name1[Size];
-// char name2[Size] = "c++programing";
+        // char name1[Size];
+        // char name2[Size] = "c++programing";
 
-// cout << "hi" << endl;
-// cout << "name : " << name2 << "\n";
-// cout << "your name?\n";
-// cin.getline(name1, Size);
-// cout << "hi " << name1;
+        // cout << "hi" << endl;
+        // cout << "name : " << name2 << "\n";
+        // cout << "your name?\n";
+        // cin.getline(name1, Size);
+        // cout << "hi " << name1;
 
-// char char1[20];
-// char char2[20] = "jauar";
+        // char char1[20];
+        // char char2[20] = "jauar";
 
-// string str1;
-// string str2 = "pan da";
+        // string str1;
+        // string str2 = "pan da";
 
-// str1 = str2;
-// cout << str1[0] <<endl;
-    
- struct my
- {
-     string name;
-     string position;
-     int age;
-     int height;
+        // str1 = str2;
+        // cout << str1[0] <<endl;
+        
+        //  struct my
+        //  {
+        //      string name;
+        //      string position;
+        //      int age;
+        //      int height;
 
- };
-        my A[2] = {
-                {"A", "A", 1, 2},
-                {"B" , "B", 3 ,4}
-        };
-        cout << A[1].age<<endl;
+        //  };
+        //         my A[2] = {
+        //                 {"A", "A", 1, 2},
+        //                 {"B" , "B", 3 ,4}
+//         };
+        //         cout << A[1].age<<endl;
+        // union myunion
+        // {
+        //         int intval;
+        //         long longval;
+        //         float floatval;
+        // };
+        // myunion test;
+        // test.intval = 3;
+        // cout << test.intval << endl;
+        // test.longval = 33;
+        // cout << test.intval << endl;
+        // cout << test.longval << endl;
+        // test.floatval= 3.3;
+        // cout << test.intval << endl;
+        // cout << test.longval << endl;
+        // cout << test.floatval << endl;
+        // int val = 3;
+
+        // std::cout << &val << std::endl;
+        
+        
+//포인터//
+
+        // int *a; //c스타일
+        // int* b; //c++ 스타일
+        // int* c,d; //c는 포인터 변수, d는 int형 변수
+
+        // int a =6;      
+        // int* b;
+
+        // b = &a;
+        // cout << "a의 값 : " << a << endl;
+        // cout << "*b의 값 : " << *b << endl;
+
+        // cout << "a의 주소 : " << &a << endl;
+        // cout << "*b의 주소 : " << b << endl;
+        
+        // *b = *b + 1;
+
+        // cout << "이제 a의 값은 : " << a <<endl;
+
+        // int a = 10;
+        // int* b;
+
+        // b = &a;
+
+        // cout << a << endl;
+        // cout << *b <<endl;
+
+        // cout << &a << endl;
+        // cout << b << endl;
+
+        // *b = *b +1;
+
+        // cout << a << endl;
+
+// new // 
+
+        // int* pointer = new int;
+        // cout<< << endl;
+        
+// delete//
+
+        // double* p3 = new double[3];
+        // p3[0] = 0.2;
+        // p3[1] = 0.5;
+        // p3[2] = 0.8;
+
+        // cout << p3[0] << endl;
+
+        // p3 = p3 + 1 ;
+        // cout << p3[0] << endl;
+
+        // p3 = p3 - 1;
+        // delete[] p3;
+
     return 0;
-    
-    }
+}
