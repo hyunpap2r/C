3f710bda1a202fc23c0e05decbf610eac42e1130
hyunpaper <hyunpaper5170@naver.com>
2022-06-16 23:49:44 +0900

    def

--- def.cpp
+++ def.cpp
@@ -6,8 +6,23 @@ using namespace std;
 void withoutreturn(){
     cout <<"반환 값이 없는 함수 입니다." << endl;
 }
+
+int withreturn(){
+    cout <<"반환값이 있는 함수 입니다." <<endl;
+    int num1 = 15;
+    return num1;
+}
+
+
+void p(int num){
+    cout << "값은" << num << "입니다" <<endl;
+}
+
 int main(){
-    withoutreturn();
+    int ret =withreturn();
+
+    p(ret);
 
     return 0;
 }
+
